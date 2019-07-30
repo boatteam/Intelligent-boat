c54fb2fdc73b30924bee4801ff429100bffd11a0
CodesCatcher <731801843@qq.com>
2019-07-30 11:22:07 +0800

    初始化项目结构

---
+++ src/main/java/edu/zust/boatserver/BoatserverApplication.java
@@ -0,0 +1,13 @@
+package edu.zust.boatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoatserverApplication.class, args);
+    }
+
+}
